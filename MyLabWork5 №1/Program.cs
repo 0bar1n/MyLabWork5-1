a0edcfad4e876032446de8c024909d697d3f1ab3
barinskaya <1388zxc@gmail.com>
2022-09-30 15:33:44 +0300

    Добавьте файлы проекта.

---
+++ MyLabWork5 №1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyLabWork5__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:"); //просим пользователя ввести число
+            double a = Double.Parse(Console.ReadLine()); //задание числа вещественного типа
+            a = Math.Round(a, 2); // округление заданного числа с точностью до сотых, присваивание значения переменной
+            Console.WriteLine($"Число  с точностью до сотых {a}"); //вывод на экран значения переменной
+        }
+    }
+}
